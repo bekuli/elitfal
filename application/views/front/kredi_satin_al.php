31abc5dad4c13dd67ffdb608793a188401418a1b
olgungurler <62409329+olgungurler@users.noreply.github.com>
2020-03-20 10:55:36 +0300

    kredi-satin-al yapıldı

--- application/views/front/kredi_satin_al.php
+++ application/views/front/kredi_satin_al.php
@@ -1,5 +1,46 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 
+<div class="container">
+	<div class="row">	
+		<div class="col-lg-12">
+			<div class="kredi-listesi">	
+				<?php 
+					foreach ($krediler as $row)
+					{
+						?>
+						<div class="col-md-3">
+							<div class="kredi-kutusu">
+						
+								<div class="kredi">
+									<?=$row["kredi"]?> Kredi
+								</div>
+
+								<div class="kredi-aciklama">
+									<?=$row["aciklama"]?>
+								</div>
+						
+								<div class="kredi-fiyat">
+									<?=$row["fiyat"]?> TL
+								</div>
+
+								<a href="">
+
+									<div class="kredi-al-btn">
+										Satın Al
+									</div>
+
+								</a>
+
+							</div>
+						</div>
+						<?php
+					}
+				?>
+			</div>
+		</div>
+	</div>	
+</div>
+
 <style>
 	body{background: #f1f1f1}
 </style>
