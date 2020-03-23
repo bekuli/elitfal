e838e7d40c4261c0cfe3f180e5157ae16ad9d933
olgungurler <62409329+olgungurler@users.noreply.github.com>
2020-03-23 20:08:35 +0300

    odeme_basarili sayfası yapıldı

--- application/views/front/odeme_basarili.php
+++ application/views/front/odeme_basarili.php
@@ -1,5 +1,35 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="check-icon-odeme">
+				<i class="fa fa-check-circle"></i>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="odeme-basarili-baslik">
+				Teşekkürler!
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="odeme-basarili-baslik">
+				Ödemeniz Başarıyla Tamamlandı
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="odeme-basarili-aciklama">
+				Ödeme metodunuz kabul edildi ve işleminiz onaylandı.
+			</div>
+		</div>
+	</div>
+</div>
 
 <style>
 	body{background: #f1f1f1}
