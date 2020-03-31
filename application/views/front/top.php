85b4aa59b9f95e271c908e12ff4b2a8fdc8488b8
Berkay Özdemir <berkayozdemir3@stu.aydin.edu.tr>
2020-03-31 03:19:48 +0300

    asd

--- application/views/front/top.php
+++ application/views/front/top.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-   	<title>FAL SİTESİNİN ADI</title>
+   	<title>Elit Fal</title>
 
    	<meta name="Language" content="turkish">
     <meta name="Copyright" content="Fal sitesi adı">
